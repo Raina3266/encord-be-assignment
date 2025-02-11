89d1a4e3bc7b3d9140f2df0f45a4ac3b61926680
raina <chenganlin990326@gmail.com>
2025-02-11 14:15:07 +0000

    column get and set

--- tetris_code/src/grid/column.rs
+++ tetris_code/src/grid/column.rs
@@ -11,5 +11,32 @@ impl Column {
     pub fn max_height(&self) -> usize {
         todo!()
     }
+    
+    pub fn get(&self, height: usize) -> bool {
+        todo!()
+    }
+
+    pub fn set(&mut self, height: usize, value: bool) {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::usize;
+
+    use super::*;
+
+    #[test]
+    fn simple_get_set() {
+        let mut col = Column::new();
+
+        // can get columns before setting
+        assert_eq!(col.get(5), false);
+        assert_eq!(col.get(usize::MAX), false);
+
+        col.set(5, true);
+        assert_eq!(col.get(5), true);
+    }
 }
 
