6fbef69a045c7a84265f2ad7daa15aee84cd9114
raina <chenganlin990326@gmail.com>
2025-02-11 16:09:41 +0000

    main loop

--- tetris_code/src/main.rs
+++ tetris_code/src/main.rs
@@ -1,9 +1,32 @@
 #![allow(clippy::bool_assert_comparison)]
 
+use std::io::{stdin, stdout, Read, Result, Write};
+
+use grid::Grid;
+use input::{Input, InputToken};
+
 mod grid;
 mod input;
 mod shape;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    main_loop(stdin(), stdout(), 10)
+}
+
+fn main_loop<R: Read, W: Write>(input: R, mut output: W, width: usize) -> Result<()> {
+    let mut input = Input::new(input);
+    let mut grid = Grid::new(width);
+
+    loop {
+        match input.next_token()? {
+            InputToken::End => return Ok(()),
+            InputToken::NewLine => {
+                writeln!(output, "{}", grid.max_height())?;
+                grid = Grid::new(width);
+            }
+            InputToken::Entry { shape, position } => {
+                grid.add_shape(shape, position);
+            }
+        }
+    }
 }
