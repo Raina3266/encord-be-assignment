cc5aef509e12cbba7133576c616e4c9b220f6e01
raina <chenganlin990326@gmail.com>
2025-02-12 14:09:47 +0000

    fix cargo doc

--- tetris_code/src/lib.rs
+++ tetris_code/src/lib.rs
@@ -14,7 +14,7 @@ mod tests;
 
 /// The main program logic
 ///
-/// This function works with anything that implements [`Read`] or [`Write`], which could be [`stdin`] or [`stdout`]
+/// This function works with anything that implements [`Read`] or [`Write`], which could be [`std::io::stdin`] or [`std::io::stdout`]
 pub fn main_loop<R: Read, W: Write>(input: R, mut output: W, width: usize) -> Result<()> {
     let mut input = Input::new(input);
     let mut grid = Grid::new(width);
