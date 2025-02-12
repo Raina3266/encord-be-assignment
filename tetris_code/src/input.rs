86dee1f07175552b7ed90f04c3ce7f92a70bb1bc
raina <chenganlin990326@gmail.com>
2025-02-12 13:15:27 +0000

    test for big input

--- tetris_code/src/input.rs
+++ tetris_code/src/input.rs
@@ -10,7 +10,7 @@ pub enum InputToken {
 }
 
 /// A wrapper around something that implements [`Read`] that produces [`InputToken`]s
-/// 
+///
 /// It's like an `Iterator`, but uses a custom function [`Input::next_token`].
 pub struct Input<R: Read> {
     // Iterator of Result<u8>
@@ -44,14 +44,15 @@ impl<R: Read> Input<R> {
         let shape = Shape::from_byte(next_byte).expect("input is valid");
         let position = self.inner.next().expect("input is valid")?;
         let position = position - b'0';
-        
-        Ok(InputToken::Entry { shape, position })
 
+        Ok(InputToken::Entry { shape, position })
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use iter_read::IterRead;
+
     use super::*;
 
     #[test]
@@ -85,5 +86,28 @@ mod tests {
         );
 
         assert_eq!(input.next_token().unwrap(), InputToken::End);
+    }
+
+    // about 18GB of input, more memory than my laptop
+    // run in release mode because it takes a minute
+    #[test]
+    fn handles_really_big_input() {
+        // make a big iterator of bytes
+        let bytes_iterator = std::iter::repeat(*b"Q0,")
+            .take(6_000_000_000)
+            .chain(Some(*b"Q0\n"))
+            .flatten();
+
+        // convert to impl Read
+        let read = IterRead::new(bytes_iterator);
+
+        let mut input = Input::new(read);
+
+        loop {
+            let token = input.next_token().unwrap();
+            if token == InputToken::End {
+                break;
+            }
+        }
     }
 }
